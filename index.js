9e67ff0f09039fac1511696310d5248639f9c93a
ianmwendwa11 <ian.musyoki12@gmail.com>
2025-03-18 10:57:36 +0300

    assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
+ ];
+
+ function displayRamens() {
+    const menu = document.getElementById("ramen-menu");
+    menu.innerHTML = ""; 
+    ramens.forEach(ramen => {
+        const img = document.createElement("img");
+        img.src = ramen.image;
+        img.alt = ramen.name;
+        img.addEventListener("click", () => displayRamenDetails(ramen));
+        menu.appendChild(img);
+    });
+}
